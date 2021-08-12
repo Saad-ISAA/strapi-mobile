aad3c8bda52f2a2334cb551f44435f7c4bd697fc
Rashid Wassan <rashidwassaan@gmail.com>
2021-08-13 00:15:58 +0500

    added Strapi switch & media library settings page

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:strapi_flutter_cms/Customwidgets/buttons.dart';
 import 'package:strapi_flutter_cms/Customwidgets/dialogs.dart';
+import 'package:strapi_flutter_cms/Customwidgets/on_off_button.dart';
 import 'package:strapi_flutter_cms/shared/colors.dart';
 import 'package:strapi_flutter_cms/shared/drawer.dart';
 import 'package:velocity_x/velocity_x.dart';
@@ -29,7 +30,7 @@ class _SettingsPageState extends State<SettingsPage> {
         ),
       ]),
       endDrawer: _buildSettingsDrawer(),
-      body: Internationalization(),
+      body: MediaLibrarySettingsPage(),
     );
   }
 
@@ -153,6 +154,119 @@ class LocaleRow extends StatelessWidget {
           )
         ],
       ).p(16),
+    );
+  }
+}
+
+class MediaLibrarySettingsPage extends StatefulWidget {
+  const MediaLibrarySettingsPage({Key key}) : super(key: key);
+
+  @override
+  State<MediaLibrarySettingsPage> createState() =>
+      _MediaLibrarySettingsPageState();
+}
+
+class _MediaLibrarySettingsPageState extends State<MediaLibrarySettingsPage> {
+  bool _responsiveFriendlyUpload;
+  bool _sizeOptimization;
+  bool _autoOrientation;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          'Media Library - Settings'.text.xl2.semiBold.make(),
+          2.heightBox,
+          'Configure the settings for media library'
+              .text
+              .medium
+              .color(neutral500)
+              .make(),
+          16.heightBox,
+          Card(
+            margin: EdgeInsets.all(0),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(3),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                'IMAGE'.text.color(neutral500).make(),
+                24.heightBox,
+                Text('Enable responsive friendly upload',
+                    style:
+                        TextStyle(fontSize: 13, fontWeight: FontWeight.w600)),
+                StrapiSwitch(
+                  val: _responsiveFriendlyUpload,
+                  onOffPress: () {
+                    setState(() {
+                      _responsiveFriendlyUpload = false;
+                    });
+                  },
+                  onOnPress: () {
+                    setState(() {
+                      _responsiveFriendlyUpload = true;
+                    });
+                  },
+                ),
+                'It automatically generates size variants of the uploaded asset.'
+                    .text
+                    .size(14)
+                    .color(neutral400)
+                    .make(),
+                24.heightBox,
+                Text('Enable size optimization (without quality loss)',
+                    style:
+                        TextStyle(fontSize: 13, fontWeight: FontWeight.w600)),
+                StrapiSwitch(
+                  val: _sizeOptimization,
+                  onOffPress: () {
+                    setState(() {
+                      _sizeOptimization = false;
+                    });
+                  },
+                  onOnPress: () {
+                    setState(() {
+                      _sizeOptimization = true;
+                    });
+                  },
+                ),
+                24.heightBox,
+                Text('Enable auto orientation',
+                    style:
+                        TextStyle(fontSize: 13, fontWeight: FontWeight.w600)),
+                StrapiSwitch(
+                  val: _autoOrientation,
+                  onOffPress: () {
+                    setState(() {
+                      _autoOrientation = false;
+                    });
+                  },
+                  onOnPress: () {
+                    setState(() {
+                      _autoOrientation = true;
+                    });
+                  },
+                ),
+                'Automatically rotate image according to EXIF tag.'
+                    .text
+                    .size(14)
+                    .color(neutral400)
+                    .make(),
+              ],
+            ).p(16),
+          ),
+          Spacer(),
+          PrimarySquareButton(
+            color: success500,
+            text: 'Save',
+          ),
+          PrimaryCancelGreySquareButton(
+            onPressed: () {},
+          )
+        ],
+      ).p(24),
     );
   }
 }
