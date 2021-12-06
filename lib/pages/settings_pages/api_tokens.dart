02ee3d1e0e5870b2bd304754f8f3bf300fedaa99
Rashid Wassan <rashidwassaan@gmail.com>
2021-12-06 19:59:29 +0500

    settings drawer design update

---
+++ lib/pages/settings_pages/api_tokens.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class APITokensScreen extends StatefulWidget {
+  const APITokensScreen({Key key}) : super(key: key);
+
+  @override
+  _APITokensScreenState createState() => _APITokensScreenState();
+}
+
+class _APITokensScreenState extends State<APITokensScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
